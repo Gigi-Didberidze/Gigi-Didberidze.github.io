3b4609ead24998a6834af4c6cb941ef93e10d33b
Gigi-Didberidze <gigididberidze01@gmail.com>
2023-02-23 00:48:18 +0100

    Added Services script

---
+++ src/Services.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Slider from 'react-slick';
+import { FaDesktop, FaMobileAlt, FaDatabase } from 'react-icons/fa';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './Services.css';
+
+const Services = () => {
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
+
+
+  return (
+    <div className="services-container">
+      <h2>Expert Services to Meet Your Needs</h2>
+      <h2>Discover my diverse range of creative services and take your personal projects to the next level</h2>
+      <div className="services-slider">
+        <Slider {...settings}>
+          <div className="service">
+            <FaDesktop className="service-icon" />
+            <h3>Full Stack Development</h3>
+            <p>Custom desktop application development using the latest technologies.</p>
+          </div>
+          <div className="service">
+            <FaMobileAlt className="service-icon" />
+            <h3>Mobile App Development</h3>
+            <p>Custom desktop application development using the latest technologies.</p>
+          </div>
+          <div className="service">
+            <FaDatabase className="service-icon" />
+            <h3>Machine Learning Engineering</h3>
+            <p>Custom desktop application development using the latest technologies.</p>
+          </div>
+          <div className="service">
+            <FaDatabase className="service-icon" />
+            <h3>Automation Engineering</h3>
+            <p>Custom desktop application development using the latest technologies.</p>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
