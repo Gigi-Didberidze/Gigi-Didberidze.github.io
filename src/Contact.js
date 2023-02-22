9c9acdab056affe98930216304883a01b8520184
Gigi-Didberidze <gigididberidze01@gmail.com>
2023-02-23 00:49:19 +0100

    Added Contact script

---
+++ src/Contact.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import emailjs from "emailjs-com";
+import './Contact.css';
+
+
+function Contact() {
+    const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      message: "",
+    });
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      emailjs
+        .sendForm(
+          "service_mgafu9c",
+          "template_9zea78m",
+          event.target,
+          "jwli9s537zAMxvA9K"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Your message has been sent successfully!");
+            setFormData({
+              name: "",
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            console.log(error.text);
+            alert("An error occurred, please try again later.");
+          }
+        );
+    };
+  
+    const handleChange = (event) => {
+      setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+  
+    return (
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Get in touch</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
+    );
+  }
+  
+  export default Contact;
